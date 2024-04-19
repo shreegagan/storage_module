7937ddc4a61919a1fabc2f20d0f5337a963cecaa
shreegagan <104724964+shreegagan@users.noreply.github.com>
2024-04-19 15:08:43 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+ resource "azurerm_resource_group" "rg" {
+   name     = var.resource_group_name
+   location = var.resource_group_location
+ }
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  # allow_blob_public_access = true
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.storage_container_name
+  storage_account_name  = azurerm_storage_account.storage.name
+  container_access_type = "container" # "blob" "private"
+}
+
+resource "azurerm_storage_blob" "blob" {
+  name                   = "sample-file.sh"
+  storage_account_name   = azurerm_storage_account.storage.name
+  storage_container_name = azurerm_storage_container.container.name
+  type                   = "Block"
+  source                 = "commands.sh"
+}
+
