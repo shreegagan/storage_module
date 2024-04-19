c0ff1ed73aa2d0f90d36e6e29081ea6db76b5682
shreegagan <104724964+shreegagan@users.noreply.github.com>
2024-04-19 15:09:54 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+#
+variable "resource_group_name" {
+  type        = string
+  description = "RG name in Azure"
+}
+
+variable "resource_group_location" {
+  type        = string
+  description = "RG location in Azure"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Storage Account name in Azure"
+}
+
+variable "storage_container_name" {
+  type        = string
+  description = "Storage Container name in Azure"
+}
+
+
