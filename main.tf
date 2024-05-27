0b31a0db0059869b4f81b348d57ba147a88c0506
shreegagan <104724964+shreegagan@users.noreply.github.com>
2024-05-27 12:14:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
- resource "azurerm_resource_group" "rg" {
-   name     = var.resource_group_name
-   location = var.resource_group_location
- }
+# resource "azurerm_resource_group" "rg" {
+#   name     = var.resource_group_name
+#   location = var.resource_group_location
+# }
 
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
